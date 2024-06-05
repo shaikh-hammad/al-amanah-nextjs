31a15a10cdaf7ddb6b2bb2f66a32cac91244d6bf
Hammad Shaikh <shaihamm@sheridancollege.ca>
2024-06-05 01:57:31 -0400

    done4

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -4,13 +4,16 @@ import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
+import Image from 'next/image';
 import logoImage from '@/public/amanah_combined.png';
 
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-      <img src={logoImage} alt="Logo" className="mx-auto mb-4" />
+      <div className="mx-auto mb-4">
+          <Image src={logoImage} alt="Logo" width={200} height={200} />
+        </div>
         <h1 className="text-lg font-semibold">
           Welcome to Al-Amanah Islamic Assistant!
         </h1>
