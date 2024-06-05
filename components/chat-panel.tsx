3a46fefa3cf302e0dc87a87978cbf34b9d378b67
Hammad Shaikh <shaihamm@sheridancollege.ca>
2024-06-05 01:53:50 -0400

    done3

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,24 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'Who was',
+      subheading: 'Prophet Nuh?',
+      message: `Who was Prophet Nuh?`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'Is it permissible',
+      subheading: 'to have a mortgage?',
+      message: 'Is it permissible to have a mortgage?'
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'Who was',
+      subheading: 'Prophet Ibrahim?',
+      message: `Who was Prophet Ibrahim?`
     },
     {
       heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      subheading: `signs of the Day of Judgment?`,
+      message: `What are some signs of the Day of Judgment?`
     }
   ]
 
--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -4,10 +4,13 @@ import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
+import logoImage from '@/public/amanah_combined.png';
+
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+      <img src={logoImage} alt="Logo" className="mx-auto mb-4" />
         <h1 className="text-lg font-semibold">
           Welcome to Al-Amanah Islamic Assistant!
         </h1>
