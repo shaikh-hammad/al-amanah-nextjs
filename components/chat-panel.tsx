315b0148495c1ddb57368cc355b0d0d6e42b3425
Hammad Shaikh <shaihamm@sheridancollege.ca>
2024-12-09 03:59:10 -0500

    loading spinner

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -117,9 +117,7 @@ export function ChatPanel({
 
         {loading && (
           <div className="flex justify-center my-4">
-            <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
+            <div className="loader"></div>
           </div>
         )}
 
